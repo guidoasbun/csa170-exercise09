8a92d949552835cb4ffc4a2b4e51b6461104a655
guidoasbun <gdrodrigo8@gmail.com>
2020-09-22 22:30:58 -0700

    First

---
+++ LetterGradeToNumber.java
@@ -0,0 +1,21 @@
+/* 
+*   Guido Asbun
+*   CS A170 
+*   22-September-2020     
+* 
+*   Exercise 07
+*/ 
+
+import java.util.Scanner;
+public class LetterGradeToNumber{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter a letter grade: ");
+       
+        String letterGrade = input.next();
+        
+        System.out.println(letterGrade);
+
+    }
+}
